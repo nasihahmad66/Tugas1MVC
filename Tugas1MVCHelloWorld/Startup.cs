769678ca3af3f9baa21a3ea8e0c78b91619a9f76
NA <NA@DESKTOP-08LF8V9>
2017-01-09 08:13:08 +0700

    Add project files.

---
+++ Tugas1MVCHelloWorld/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tugas1MVCHelloWorld.Startup))]
+namespace Tugas1MVCHelloWorld
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
